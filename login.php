a005c8461e9778dd648b782c510b953c4f035b7d
Silvestrs <SilvestrsL47@gmail.com>
2023-10-16 12:18:55 +0300

    Silvja Stils + Mobile View + Login/Register

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="globalStyle.css">
+    <title>Pasta Hub Login</title>
+</head>
+<body>
+    <main>
+        <header class="header">
+            <span>
+                <p>Pasta HUB</p>
+            </span>
+            <span>
+                <a href="./login.php">Ieiet</a>
+                <a href="./register.php">Reģistrēties</a>
+            </span>
+            <span>
+                <img src="./media/logo.png">
+            </span>
+        </header>
+        <div class="flex cen col regDisplay">
+        <label>Lietotājvārds:</label>
+            <input type="text" name="" id="">
+            <label>Parole:</label>
+            <input type="password" name="" id="">
+            <input type="button" value="Ieiet">
+        </div>
+    </main>
+</body>
+</html>
