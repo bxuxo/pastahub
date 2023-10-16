a6d364596536078aab6d7053779ff88618d247e6
Bruno <bruno.banionis.b@gmail.com>
2023-10-16 09:26:55 +0300

    structure

--- index.php
+++ index.php
@@ -1,10 +1,29 @@
 <?php
 
-$page = $_GET['page_name'];
-if ($page == 'index.php') {
-    $page = 'index';
+$page = $_GET["page_name"];
+if ($page == "index.php") {
+    $page = "index";
 }
 
-echo $page;
+$pageFolderPath = "./pages/" . $page . "Page";
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Pasta HUB</title>
+
+        <link rel="stylesheet" href="styles.css">
+        <link rel="stylesheet" href="<?= $pageFolderPath . "/style.css" ?>">
+    </head>
+    <body>
+        <?php 
+            include "$pageFolderPath/content.php"; 
+        ?>
+    </body>
+
+    <script src="<?= $pageFolderPath . "/script.js" ?>"></script>
+</html>
