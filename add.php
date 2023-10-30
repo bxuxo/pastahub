4bc347ef08d3b07408afe5a491d34111f6a3a97c
Silvestrs <SilvestrsL47@gmail.com>
2023-10-30 11:13:30 +0200

    Update add.php

--- add.php
+++ add.php
@@ -33,7 +33,7 @@
             </select>
             <textarea name="" id="" cols="30" rows="10" placeholder="Apraksts"></textarea>
             <label>Bilde:</label>
-            <!-- <input type="file" name="" id=""> -->
+
             <label for="file-upload" class="custom-file-upload">
                 Izvēlieties bildi
             </label>
