b39c4369da352b8ad845832bd6613e78f94096a1
Silvestrs <SilvestrsL47@gmail.com>
2023-10-30 11:35:12 +0200

    Lapsins gribeja file upload visual feedback.
    Lapsins gribeja file upload visual feedback.

--- add.php
+++ add.php
@@ -1,5 +1,15 @@
 <?php
-
+// if(isset($_FILES["pictures"])){
+//     foreach ($_FILES["pictures"]["error"] as $key => $error) {
+//         if ($error == UPLOAD_ERR_OK) {
+//             $tmp_name = $_FILES["pictures"]["tmp_name"][$key];
+//             // basename() may prevent filesystem traversal attacks;
+//             // further validation/sanitation of the filename may be appropriate
+//             $name = basename($_FILES["pictures"]["name"][$key]);
+//             echo $name;
+//         }
+//     }
+// }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -7,9 +17,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="globalStyle.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <title>Pasta Hub Login</title>
 </head>
 <body>
+    <script>
+        function getFileData(myFile){
+            var file = myFile.files[0];  
+            var filename = file.name;
+            $('.custom-file-upload').html(filename);
+        }
+    </script>
     <main>
         <header class="header">
             <span>
@@ -34,11 +52,13 @@
             <textarea name="" id="" cols="30" rows="10" placeholder="Apraksts"></textarea>
             <label>Bilde:</label>
 
-            <label for="file-upload" class="custom-file-upload">
-                Izvēlieties bildi
-            </label>
-            <input id="file-upload" type="file"/>
-            <button>Pievienot</button>
+            <form method="post" enctype="multipart/form-data">
+                <label for="file-upload" class="custom-file-upload">
+                    Izvēlieties bildi
+                </label>
+                <input id="file-upload" type="file" onchange="getFileData(this);" />
+                <button>Pievienot</button>
+            </form>
         </div>
     </main>
 </body>
